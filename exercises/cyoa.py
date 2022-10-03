637d43928eb86c19296502bdf68e867beb89ceb9
Foluwa Agbebi <magbebi@unc.edu>
2022-10-03 13:39:50 -0400

    Progress on EX06

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -38,7 +38,6 @@ def main() -> None:
         if user_choice == choice_one:
             if first_time:
                 print(f"\nSeems like a good place to start! Let's put our facilites to the test eh {WINKING_EMOJI}?")
-                print("Speaking of which... what do you want to test first?")
             test_library()
             print("\nWhat do you wanna do now?")
             first_time = False
@@ -48,6 +47,8 @@ def main() -> None:
             global points
             points += test_dorms(points)
             print(f"\nYou've earned ${points} in lawsuit money.")
+            if points >= 1000:
+                print("\nAt this rate, you're gonna be rolling in dough after four years of undergrad!")
             print("\nWhat do you wanna do now?")
             first_time = False
         elif user_choice == choice_three:
@@ -81,7 +82,7 @@ def test_library() -> None:
         else:
             fountain_test(fountain_results[i])
     else:
-        i: int = randint(0, len(sink_results) - 1)
+        i = randint(0, len(sink_results) - 1)
         sink_test(sink_results[i])
         
     
@@ -95,7 +96,7 @@ def fountain_test(result: float):
         print(f"\nSURPRISE! Upon retesting, lead levels jumped from 185.0 to {result} ppb, ~{factor} times the EPA minimum regulated amount!\nThat's a lot of lead!!!!")
     else:
         print(f"\nLead levels in this fountain are {result} ppb, which is ~{factor} times the EPA minimum regulated amount!")
-    print(f"You have received ${points} in lawsuit money.")
+    print(f"\nYou have received ${points} in lawsuit money.")
     if points >= 1000:
         print("\nAt this rate, you're gonna be rolling in dough after four years of undergrad!")
 
@@ -114,35 +115,43 @@ def sink_test(result: float):
 
 def test_dorms(x: int) -> int:
     """Function that calculates the "lawsuit money" based on how old the user-inputted dorm is, and updates the player value accordingly."""
-    dorm_names_years: tuple[str, int] = ["Alderman", 1937, "Alexander", 1939, "Avery", 1958, "Carmichael", 1986, "Cobb", 1952, "Conner", 1948, "Craige", 1962, "Craige North", 2002, "Ehringhaus", 1962, "Everett", 1928, "Graham", 1924, "Grimes", 1922, "Hardin", 2002, "Hinton James", 1967, "Horton", 2002, "Joyner", 1947, "Kenan", 1939, "Koury", 2002, "Lewis", 1924, "Magnum", 1922, "Manly", 1922, "McClinton", 1924, "McIver", 1939, "Morrison", 1965, "Old East", 1795, "Old West", 1823, "Parker", 1958, "Rams 1", 2006, "Rams 2", 2006, "Rams 3", 2006, "Rams 4", 2006, "Rams 5", 2006, "Stacy", 1938, "Spencer", 1924, "Teague", 1958, "Ruffin", 1922, "Winston", 1948]
-    dorm_input: str = input("\nWhat dorm do you live in?\n> ")
+    dorm_names: list[str] = ["Alderman", "Alexander", "Avery", "Carmichael", "Cobb", "Conner", "Craige", "Craige North", "Ehringhaus", "Everett", "Graham", "Grimes", "Hardin", "Hinton James", "Horton", "Joyner", "Kenan", "Koury", "Lewis", "Magnum", "Manly", "McClinton", "McIver", "Morrison", "Old East", "Old West", "Parker", "Rams 1", "Rams 2", "Rams 3", "Rams 4", "Rams 5", "Stacy", "Spencer", "Teague", "Ruffin", "Winston"]
+    dorm_years: list[int] = [1937, 1939, 1958, 1986, 1952, 1948, 1962, 2002, 1962, 1928, 1924, 1922, 2002, 1967, 2002, 1947, 1939, 2002, 1924, 1922, 1922, 1924, 1939, 1965, 1795, 1823, 1958, 2006, 2006, 2006, 2006, 2006, 1938, 1924, 1958, 1922, 1948]
+    dorm_input: str = input("\nWhat dorm are you testing?\n> ")
     i: int = 0
-    while i < len(dorm_names_years):
-        if dorm_names_years[i] == dorm_input:
-            print(f"\nThis dorm was built in {dorm_names_years[i + 1]}!")
-            if dorm_names_years[i + 1] == 1795:
-                print("WOW, The Big Kahuna! This is the oldest dorm on campus, meaning it has the highest lead levels of them all. Who needs a scholarship when you've got all this money!?")
+    while i < len(dorm_names):
+        if dorm_names[i] == dorm_input:
+            print(f"\nThis dorm was built in {dorm_years[i]}!")
+            if dorm_years[i] == 1795:
+                print("WOW, The Big Kahuna! This is the oldest dorm on campus, meaning it has the highest lead levels of them all.\nWho needs a scholarship when you've got all this money!?")
                 global points
-                return 10000
-            elif 1900 < dorm_names_years[i + 1] < 1930:
+                test_dorm_output: int = 10000
+                i += 1
+            elif 1900 < dorm_years[i] < 1930:
                 print("That's one the oldest dorms on campus, so it's likely to have the most lead! Lucky you!")
-                return randint(4001, 6000)
-            elif 1931 < dorm_names_years[i + 1] < 1960:
+                test_dorm_output = randint(667, 1000)
+                i += 1
+            elif 1931 < dorm_years[i] < 1960:
                 print("That dorm's pretty old, but not the oldest there is. You're only getting moderate amount of lead!")
-                return randint(2001, 4000)
-            elif 1961 < dorm_names_years[i + 1] < 1990:
+                test_dorm_output = randint(334, 666)
+                i += 1
+            elif 1961 < dorm_years[i] < 1980:
                 print(f"You're on South campus eh? That's gonna be slim pickings; you're probably below the EPA minimum {SHRUGGING_EMOJI}")
-                return randint(0, 2000)
-            elif dorm_names_years[i + 1] > 1991:
+                test_dorm_output = randint(1, 333)
+                i += 1
+            elif dorm_years[i] > 1981:
                 print("Dang, you're living in one of the modern dorms. No lead (or money) for you; better luck next year!")
-                return 0
-        i += 2
+                test_dorm_output = 0
+                i += 1
+        else:
+            i += 1
+    return test_dorm_output
 
 
 def exit_game() -> None:
     """Function to end the game when the player chooses the ignore the issue."""
     print(f"\nYou're right {player}, so what if there's a little lead poisoning? Kids these days have it too easy anyway!")
-    print(f"You earned ${points} in lawsuit money.")
+    print(f"\nYou earned ${points} in lawsuit money.\n")
 
 
 if __name__ == "__main__":
